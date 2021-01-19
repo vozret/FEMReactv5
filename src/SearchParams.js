05ceaa8b3e142fafcc47cdb528be51c135b8ab42
Veronika Ozretic <vozret00@fesb.hr>
2021-01-19 12:46:10 +0100

    useEffect()

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+// pet is a client
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./Dropdown";
 
 const SearchParams = () => {
@@ -9,10 +10,26 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
   // hooks never go inside of if statements or for loops!!!!
   //console.log("state of location: " + location);
+
+  // useEffect happens AFTER the render
+  // you don't want to wait for the first render
+  // happens whenever we change the breeds
+  // we have to clear whatever there was before
+  // if we want to useEffect run only once, we give [] as dependency array
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      // we are destructuring the name
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
